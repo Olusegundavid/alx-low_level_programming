3b00054e194a40f92ecb0adf102cd6a5e8209850
olusegundavid <olusegundavid33@gmail.com>
2022-04-12 13:23:52 -0700

    alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,11 +5,9 @@
  */
 void print_alphabet(void)
 {
-char c = 'a';
- while (c <= 'z')
-{
-_putchar(c);
-c++;
-}
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
 _putchar('\n');
 }
+
