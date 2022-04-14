e02cd328215587642d882c19af3b67b191671499
olusegundavid <olusegundavid33@gmail.com>
2022-04-14 12:00:08 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,11 +4,11 @@
 * followed by a new line
 */
 void print_numbers(void)
-{  
+{
 int i;
-for (i = 0; i < 10; i++)    
-{      
+for (i = 0; i < 10; i++)
+{
 _putchar(i + '0');
-}  
+}
 _putchar('\n');
 }
